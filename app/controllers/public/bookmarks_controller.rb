ea88574f63ee8387878275e050597a29b47fa4c7
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-04-20 12:53:32 +0000

    [add]search一覧のViewを修正_ブックマークに関するif文の修正

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -3,24 +3,27 @@ class Public::BookmarksController < ApplicationController
   def create
     bookmark = Bookmark.new(
       user_id: current_user.id,
-      comic_id: params[:comic_id]
+      comic_id: params[:comic_id],
       )
       
     if bookmark.save!
-      redirect_to bookmark_comics_path
+      redirect_to search_index_comics_path(keyword: session["search_keyword"])
     end
   end
   
-  def destroy
-    @comic = RakutenWebService::Books::Book.search(size:9)
-    bookmark = @comic.bookmarks.find_by(user_id: current_user)
-    if bookmark.present?
-        bookmark.destroy
-        redirect_to search_index_comics_path
-    else
-        redirect_to search_index_comics_path
-    end
-  end
+  # def destroy
+  #   bookmark = Bookmark.find_by(
+  #     user_id: current_user.id, comic_id
+  #     comic_id: params[:comic_id]
+  #     )
+      
+  #   if bookmark.present?
+  #       bookmark.destroy
+  #       redirect_to bookmark_comics_path
+  #   else
+  #       redirect_to bookmark_comics_path
+  #   end
+  # end
   
   private
 
