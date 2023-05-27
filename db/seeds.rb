ccf2437708486aab40fbac7c68f2f9a33cdbabdf
EC2 Default User <ec2-user@ip-172-31-38-96.ap-northeast-1.compute.internal>
2023-05-27 10:50:39 +0000

    [fix]デプロイ後修正_seedファイルの内容追加⑵

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,22 @@
 #     password: 'n10aru203320',
 # )
 
-admins = [
-    {email: 'narumi1years.years020@gmail.com', password: 'n10aru203320'},
-]
+# admins = [
+#     {email: 'narumi1years.years020@gmail.com', password: 'n10aru203320'},
+# ]
 
-admins.each do |admin|
-    admin_data = Admin.find_by(email: admin[:email])
-    if admin_data.nil?
-        Admin.create(email: admin[:email], password: admin[:password])
-    end
-end
+# admins.each do |admin|
+#     admin_data = Admin.find_by(email: admin[:email])
+#     if admin_data.nil?
+#         Admin.create(email: admin[:email], password: admin[:password])
+#     end
+# end
 
-sites = ['コミック！', 'COMIC漫画', 'Comic Comic']
-sites.each do |site|
-    Site.find_or_create_by(name: site)
-    # モデル.find_or_create_by(条件, ブロック引数)...条件を指定して初めの1件を取得し1件もなければ作成
-end
-
-user = User.create!(
-  nick_name: "HYUNG's",
-  email: "sample320@gmail.com",
-  password: "sample", 
-  password_confirmation: "sample",
-)
-user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user1.png')), filename: 'user1.png')
+# sites = ['コミック！', 'COMIC漫画', 'Comic Comic']
+# sites.each do |site|
+#     Site.find_or_create_by(name: site)
+#     # モデル.find_or_create_by(条件, ブロック引数)...条件を指定して初めの1件を取得し1件もなければ作成
+# end
 
 # user = User.create!(
 #   nick_name: "HYUNG's",
@@ -44,16 +36,74 @@ user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user1
 # )
 # user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user1.png')), filename: 'user1.png')
 
-# User.create!(
-#   user_id: 3,
-#   nick_name: "NANA's",
-#   email: "sample77@gmail.com",
-#   profile_image: File.open("./app/assets/images/no_image.jpg")
-# )
+user = User.create!(
+  nick_name: "LEE'know",
+  email: "sample1025@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user2.png')), filename: 'user2.png')
 
-# User.create!(
-#   user_id: 4,
-#   nick_name: "Han_J",
-#   email: "sample000@gmail.com",
-#   profile_image: File.open("./app/assets/images/no_image.jpg")
-# )
+user = User.create!(
+  nick_name: "NANA",
+  email: "sample77@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user3.png')), filename: 'user3.png')
+
+user = User.create!(
+  nick_name: "Han_j",
+  email: "sample000@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user4.png')), filename: 'user4.png')
+
+user = User.create!(
+  nick_name: "F.bird",
+  email: "sample915@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user5.png')), filename: 'user5.png')
+
+user = User.create!(
+  nick_name: "Marioo'z",
+  email: "sample1230@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user6.png')), filename: 'user6.png')
+
+user = User.create!(
+  nick_name: "???BLOCK",
+  email: "sample???@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user7.png')), filename: 'user7.png')
+
+user = User.create!(
+  nick_name: "jo_jo",
+  email: "sample5@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user8.png')), filename: 'user8.png')
+
+user = User.create!(
+  nick_name: "Rambo<3",
+  email: "sample3030@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user9.png')), filename: 'user9.png')
+
+user = User.create!(
+  nick_name: "MIYAmoto",
+  email: "sample38@gmail.com",
+  password: "sample", 
+  password_confirmation: "sample",
+)
+user.profile_image.attach(io: File.open(Rails.root.join('app/assets/images/user10.png')), filename: 'user10.png')
